cf0a8786be6c9add7b7607ec254f7a44206cf752
Wmaxlees <max.lees@gmail.com>
2017-10-03 13:24:04 -0600

    Fixed filenames in example.py

--- example/example.py
+++ example/example.py
@@ -1,5 +1,5 @@
 from tensorflowdsl import runner
 
 my_runner = runner.Runner()
-my_runner.load_hyperparameters_from_json('./test.hyp')
-my_runner.load_from_file('./test.mdl')
+my_runner.load_hyperparameters_from_json('./example.hyp')
+my_runner.load_from_file('./example.mdl')
